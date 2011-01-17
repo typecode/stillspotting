991d7ae769dd104fd88d43c197258df7e97a33f5
Andrew Mahon (Type/Code) <andrew@typeslashcode.com>
2011-01-17 14:15:47 -0500

    started work on Flickr COnnectiosn

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ import hashlib
 import connections.nyt.article
 import connections.nyt.community
 import connections.nyc311.noiseComplaints
+import connections.flickr.photos.geo.photosForLocation
 
 sys.path.append("lib")
 import tornado.httpserver
@@ -42,14 +43,18 @@ connections = {
   'generic' : connections.connection.Connection({
     'database':database
   }),
-  'nytarticle':connections.nyt.article.Article({
+  'nytarticle': connections.nyt.article.Article({
     'api_key':'1b9f2c309a5bb7426b1aa181b2f1a1cc:13:62473522'
   }),
-  'nytcommunity':connections.nyt.community.Community({
+  'nytcommunity': connections.nyt.community.Community({
     'api_key':'3cd7b97dd0c16c8523ea7ccba7f5fdd1:13:49052537'
   }),
-  'googlegeocode':connections.google.geocoder.Geocoder({}),
-  'nyc311noiseComplaints':connections.nyc311.noiseComplaints.NoiseComplaints({})
+  'googlegeocode': connections.google.geocoder.Geocoder({}),
+  'nyc311noiseComplaints': connections.nyc311.noiseComplaints.NoiseComplaints({}),
+  'flickrimages': connections.flickr.photos.geo.photosForLocation.PhotosForLocation({
+    'api_key':'9ce620aec9b49f8de2cafa5144ab3876',
+    'secret':'05b101b95c7e75d6'
+  })
 }
 
 tornado_settings = {
